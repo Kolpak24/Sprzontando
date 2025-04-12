3ac144f3f2dbf0ac22d4124d647e9a894859128b
Mateusz Jablonski <96426962+PanJabuszko@users.noreply.github.com>
2025-04-13 00:24:30 +0200

    cos tam w summary

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SprzontandoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes([
+    'verify' => true
+]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+Route::get('/profile', [Controller::class, 'edit'])->name('profile.edit')->middleware('auth');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [SprzontandoController::class, 'edit'])->name('profile.edit');
+    Route::post('/profile', [SprzontandoController::class, 'update'])->name('profile.update');
+});
