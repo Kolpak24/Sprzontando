1c94754e351458df034f8207d1b60556704d6b31
Staminek <damurze@gmail.com>
2025-06-12 05:23:05 +0200

    Update adminpanel.blade.php

--- resources/views/profile/adminpanel.blade.php
+++ resources/views/profile/adminpanel.blade.php
@@ -10,7 +10,6 @@
     <div class="mb-4">
         <nav class="nav nav-pills">
             <a class="nav-link" href="{{ route('adminpanel' ) }}">Zgłoszone oferty</a>
-            {{-- <a class="nav-link" href="{{ route('showUser') }}">Oceny</a> --}}
             <a class="nav-link" href="{{ route('statystyki') }}">Statystyki</a>
         </nav> 
 </div>
